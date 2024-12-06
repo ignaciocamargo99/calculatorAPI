cb847672faeb87300095a2d90f5176851a6a7b41
ignaciocamargo99 <ignaciocamargo346@gmail.com>
2024-12-06 12:30:50 -0300

    ignore build folder

--- build/routes/calculator.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const calculator_controller_1 = require("../controllers/calculator.controller");
-const router = (0, express_1.Router)();
-router.post('/calculate', calculator_controller_1.calculatorController);
-exports.default = router;
